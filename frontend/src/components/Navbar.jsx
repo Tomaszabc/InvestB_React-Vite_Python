758fa253035eba0c524a38a773658c5d6dc29bcc
Tomaszabc <tomasz.borowiec1@pollub.edu.pl>
2025-08-16 09:09:26 +0200

    navbar visible

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -2,13 +2,8 @@ import React from "react";
 
 const Navbar = () => {
   return (
-    <nav className="w-full h-48 md:h-64 lg:h-80 relative overflow-hidden">
-      <img
-        src="/top-bg.webp"
-        alt="Navbar Background"
-        className="absolute inset-0 w-full h-full object-cover object-center z-0"
-      />
-      {/* Możesz dodać tutaj elementy nawigacyjne */}
+    <nav className="w-full h-24 flex items-center justify-center bg-gray-800 border-b-4 border-red-500">
+      <h1 className="text-4xl font-bold text-white">Uranium Counter</h1>
     </nav>
   );
 };
