a0d4061846f8d7d4278a14231f07100acc875b35
Tomaszabc <tomasz.borowiec1@pollub.edu.pl>
2025-08-16 09:27:01 +0200

    navbar style

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -2,7 +2,16 @@ import React from "react";
 
 const Navbar = () => {
   return (
-    <nav className="w-full h-24 flex items-center justify-center bg-gray-800 border-b-4 border-red-500">
+    <nav
+      className="navbar h-16 flex items-center justify-center "
+      style={{
+        width: "100%",
+        backgroundImage: "url('backgroung-simple.jpg')",
+        backgroundSize: "cover",
+        backgroundPosition: "center",
+        backgroundRepeat: "no-repeat",
+      }}
+    >
       <h1 className="text-4xl font-bold text-white">Uranium Counter</h1>
     </nav>
   );
